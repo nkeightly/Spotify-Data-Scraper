4aeb53759f1646af15197123e2074521c3d94f18
Keightly Mabasa <ST10218758@vcconnect.edu.za>
2023-02-24 02:47:56 +0200

    Add data analysis functions and visualizations
    In this commit, I added several functions to analyze the scraped Spotify data and generate visualizations to identify trends and patterns in user behavior. The main functions include:
    
    - `group_data_by_artist()`: This function groups the data by artist and calculates the mean popularity score for each artist.
    - `group_data_by_genre()`: This function groups the data by genre and calculates the total number of songs in each genre.
    - `plot_artist_popularity()`: This function generates a bar chart of the average popularity by artist.
    - `plot_genre_distribution()`: This function generates a pie chart of the distribution of songs by genre.
    
    I also added a README.md file to the `visualizations` folder, which includes descriptions and explanations for each visualization. Overall, these changes should make it easier to analyze and understand the Spotify data.

---
+++ analyze_data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load the scraped data into pandas dataframes
+playlist_df = pd.read_csv('data/playlists.csv')
+song_df = pd.read_csv('data/songs.csv')
+artist_df = pd.read_csv('data/artists.csv')
+
+# Group the data by artist and get the mean popularity score for each artist
+artist_popularity = artist_df.groupby('artist')['popularity'].mean()
+
+# Sort the data in descending order of popularity and plot a bar chart
+artist_popularity_sorted = artist_popularity.sort_values(ascending=False)
+artist_popularity_sorted.plot(kind='bar
